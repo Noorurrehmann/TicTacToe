232620084008ea167b8b68fe20c72e8ab780cf88
Noor Ur Rehman <163124024+Noorurrehmann@users.noreply.github.com>
2024-11-06 12:47:10 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import TicTacToe from './components/TictacToe/TicTacToe'
+
+function App() {
+
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  )
+}
+
+export default App
